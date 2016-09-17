970b58e0c41ca888740b9a1549d8694120d02684
kaorun343 <kaoru.h65536@live.jp>
2016-09-17 18:44:20 +0900

    Webpackをアップデート

--- src/vuex/mutation-types.js
+++ src/vuex/mutation-types.js
@@ -1,15 +1,15 @@
-/*eslint semi: ["error", "always"]*/
-export const ADD_ALBUMS = 'ADD_ALBUMS';
 
-export const OPEN_EDITOR = 'OPEN_EDITOR';
-export const CLOSE_EDITOR = 'CLOSE_EDITOR';
+export const ADD_ALBUMS = 'ADD_ALBUMS'
 
-export const SHOW_ALBUM = 'SHOW_ALBUM';
-export const NEW_ALBUM = 'NEW_ALBUM';
-export const CREATE_ALBUM = 'CREATE_ALBUM';
-export const EDIT_ALBUM = 'EDIT_ALBUM';
-export const UPDATE_ALBUM = 'UPDATE_ALBUM';
-export const DESTROY_ALBUM = 'DESTROY_ALBUM';
+export const OPEN_EDITOR = 'OPEN_EDITOR'
+export const CLOSE_EDITOR = 'CLOSE_EDITOR'
 
-export const CHANGE_ALBUM = 'CHANGE_ALBUM';
-export const CHECK_ALBUM = 'CHECK_ALBUM';
+export const SHOW_ALBUM = 'SHOW_ALBUM'
+export const NEW_ALBUM = 'NEW_ALBUM'
+export const CREATE_ALBUM = 'CREATE_ALBUM'
+export const EDIT_ALBUM = 'EDIT_ALBUM'
+export const UPDATE_ALBUM = 'UPDATE_ALBUM'
+export const DESTROY_ALBUM = 'DESTROY_ALBUM'
+
+export const CHANGE_ALBUM = 'CHANGE_ALBUM'
+export const CHECK_ALBUM = 'CHECK_ALBUM'
