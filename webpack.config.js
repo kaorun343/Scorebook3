f17c30f17a1353a89fa6f9a7b838a797cf31a6a5
kaorun343 <kaoru.h65536@live.jp>
2016-12-07 03:13:01 +0900

    パッケージをアプデート

--- webpack.config.js
+++ webpack.config.js
@@ -10,33 +10,33 @@ module.exports = {
     filename: 'bundle.js'
   },
   resolve: {
-    extensions: ['', '.ts', '.js']
+    extensions: ['.ts', '.js']
   },
   module: {
     loaders: [
-      { test: /\.ts$/, loader: 'ts' },
-      { test: /\.html$/, loader: 'vue-template' },
-      { test: /\.css$/, loaders: ['style', 'css'] },
-      { test: /\.scss$/, loader: ExtractTextPlugin.extract(['css', 'sass']) },
+      { test: /\.ts$/, loader: 'ts-loader' },
+      { test: /\.html$/, loader: 'vue-template-loader' },
+      { test: /\.css$/, loader: ExtractTextPlugin.extract(['css-loader']) },
+      { test: /\.scss$/, loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader']) },
       {
         test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url',
+        loader: 'url-loader',
         query: { limit: 10000, mimetype: 'application/font-woff' }
       },
       {
         test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url',
+        loader: 'url-loader',
         query: { limit: 10000, mimetype: 'application/font-woff' }
       },
       {
         test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url',
+        loader: 'url-loader',
         query: { limit: 10000, mimetype: 'application/octet-stream' }
       },
-      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader' },
       {
         test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url',
+        loader: 'url-loader',
         query: { limit: 10000, mimetype: 'image/svg+xml' }
       }
     ]
